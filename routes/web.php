b5c1a23b4e67eefcc6fd6d0ff8420113d89c7dbd
y-tajima <y-tajima@semo.co.jp>
2024-10-23 01:47:45 +0900

    adminLTEダッシュボード追加

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('adminlte');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
